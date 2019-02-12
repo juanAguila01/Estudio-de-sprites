c37703d0a6473605e7618fafb51c4a6c8e2a8a14
Juan Aguila <juan_aguilac13@hotmail.com>
2019-02-12 09:16:51 -0300

    Realizo efecto de movimiento por un fondo mas grande que el canvas haciendo scroll

--- js/EstudioSprites.js
+++ js/EstudioSprites.js
@@ -1,4 +1,121 @@
 let frame = requestAnimationFrame;
+
+let singleton = (function(){
+    let instance;
+    function crearSingleton(){
+        //privado
+        function update(){
+            frame(update);
+            //mueve el personaje en todas las direcciones.
+            if (movimientoTeclado.arriba) {
+                cat.y -= cat.vy;
+            }
+            if (movimientoTeclado.abajo) {
+                cat.y += cat.vy;
+            }
+            if (movimientoTeclado.izquierda) {
+                cat.x -= cat.vx;
+            }
+            if (movimientoTeclado.derecha) {
+                cat.x += cat.vx;
+            }
+            // colisiona al personaje con los bordes del mundo.
+            if (cat.x < mundoDeJuego.x) {
+                cat.x = mundoDeJuego.x;
+            }
+            if (cat.y < mundoDeJuego.y) {
+                cat.y = mundoDeJuego.y;
+            }
+            if (cat.x + cat.w > mundoDeJuego.w) {
+                cat.x = mundoDeJuego.w - cat.w;
+            }
+            if (cat.y + cat.h > mundoDeJuego.h) {
+                cat.y = mundoDeJuego.h - cat.h;
+            }
+            //centra la camara con respecto al personaje.
+            camara.x = Math.floor(cat.x + (cat.w / 2) - (camara.w / 2));
+            camara.y = Math.floor(cat.y + (cat.h / 2) - (camara.h / 2));
+
+            //colisiones de la camara con respecto al mundo.
+            if (camara.x < mundoDeJuego.x) {
+                camara.x = mundoDeJuego.x;
+            }
+            if (camara.y < mundoDeJuego.y) {
+                camara.y = mundoDeJuego.y;
+            }
+            if (camara.x + camara.w > mundoDeJuego.x + mundoDeJuego.w) {
+                camara.x = mundoDeJuego.x + mundoDeJuego.w - camara.w;
+            }
+            if (camara.y + camara.h > mundoDeJuego.h) {
+                camara.y = mundoDeJuego.h - camara.h;
+            }
+            
+            render();
+        }
+
+        function render(){
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.save();
+
+            context.translate(-camara.x, -camara.y);
+                        
+
+            if (sprites.length !== 0) {
+                for (let i = 0; i < sprites.length; i++) {
+                    let sprite = sprites[i];
+                       
+                    context.drawImage(image,
+                                    sprite.sourceX, sprite.sourceY,
+                                    sprite.sourceWidth, sprite.sourceHeight,
+                                    sprite.x, sprite.y,sprite.w,sprite.h);
+                       
+                } 
+            }
+            context.restore();
+        }
+        return {
+            //publico
+            loadHandler(){
+                //imagenesCargadas se asegura de que todas las imagenes del juego sean cargadas
+                //antes de que el juego comienze eliminando la posibilidad de que 
+                //alguna imagen no sea cargada antes de que el codigo corra.
+                imagenesCargadas += 1;
+                if (imagenesCargadas === 1) {
+                    update();
+                }
+            },
+            keyDownHandler(e){
+                e.preventDefault();
+                console.log(e.keyCode)
+                switch (e.keyCode) {
+                    case keyCodeTeclado.arriba: movimientoTeclado.arriba = true; break;
+                    case keyCodeTeclado.abajo: movimientoTeclado.abajo = true; break;
+                    case keyCodeTeclado.izquierda: movimientoTeclado.izquierda = true; break;
+                    case keyCodeTeclado.derecha: movimientoTeclado.derecha = true; break;
+                }
+            },
+            keyUpHandler(e){
+                e.preventDefault();
+                switch (e.keyCode) {
+                    case keyCodeTeclado.arriba: movimientoTeclado.arriba = false; break;
+                    case keyCodeTeclado.abajo: movimientoTeclado.abajo = false; break;
+                    case keyCodeTeclado.izquierda: movimientoTeclado.izquierda = false; break;
+                    case keyCodeTeclado.derecha: movimientoTeclado.derecha = false; break;
+                }
+            }
+        }
+    }
+    return{
+        getInstance(){
+            if (!instance) {
+                instance = crearSingleton();
+            }
+            console.log(instance);
+            return instance;
+        }
+    }
+})();
+
 class spritObject  {
     constructor(sourceX,sourceY,sourceWidth,sourceHeight,x,y,width,height){
         this.sourceX = sourceX;
@@ -7,15 +124,42 @@ class spritObject  {
         this.sourceHeight = sourceHeight;
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
-        this.movX = undefined;
-        this.visible = true;
-        this.rotar = 0;
-        this.alpha = 1;
-        this.sombra = false;
+        this.w = width;
+        this.h = height;
+        this.vx = 4;
+        this.vy = 4;
+        // this.movX = undefined;
+        // this.visible = true;
+        // this.rotar = 0;
+        // this.alpha = 1;
+        // this.sombra = false;
     }
 }
+
+class mundoCamara {
+    constructor(x,y,w,h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+}
+
+let movimientoTeclado = {
+    arriba : false,
+    abajo : false,
+    izquierda : false,
+    derecha : false
+}
+
+let keyCodeTeclado = {
+    arriba : 38,
+    abajo : 40,
+    izquierda : 37,
+    derecha : 39
+}
+
+let manejadorJuego = singleton.getInstance();
 
 let imagenesCargadas = 0;
 let canvas = document.getElementById('canvas');
@@ -23,162 +167,24 @@ let context = canvas.getContext('2d');
 
 let sprites = [];
 
-let escena = new spritObject(0,64,canvas.width,canvas.height,0,0,canvas.width,canvas.height);
-sprites.push(escena);
+let fondo = new spritObject(0,64,2561,1922,0,0,2561,1922);
+sprites.push(fondo);
 
-let cat = new spritObject(0,0,64,64,0,168,64,64);
+let mundoDeJuego = new mundoCamara(0,0,fondo.w,fondo.h);
+let camara = new mundoCamara(0,0,canvas.width,canvas.height);
+camara.x = Math.floor(mundoDeJuego.x + (mundoDeJuego.w / 2) - (camara.w / 2));
+camara.y = Math.floor(mundoDeJuego.y + (mundoDeJuego.h / 2) - (camara.h / 2));
+
+let cat = new spritObject(0,0,64,64,0,0,64,64);
+cat.x = Math.floor(mundoDeJuego.x + (mundoDeJuego.w / 2) - (cat.w / 2));
+cat.y = Math.floor(mundoDeJuego.y + (mundoDeJuego.h / 2) - (cat.h / 2));
 sprites.push(cat);
 
-let tiger = new spritObject(64,0,64,64,300,336,64,64);
-sprites.push(tiger);
 
 let image = new Image();
-image.src = 'img/sprites.png';
-image.addEventListener('load', loadHandler, false);
+image.src = 'img/phobosTileSheet.png';
+image.addEventListener('load', manejadorJuego.loadHandler, false);
 
-let botones = {
-    izquierda : document.getElementById('izquierda'),
-    derecha : document.getElementById('derecha'),
-    arriba : document.getElementById('arriba'),
-    abajo : document.getElementById('abajo'),
-    agranda : document.getElementById('agranda'),
-    achica : document.getElementById('achica'),
-    invisible : document.getElementById('invisible'),
-    visible : document.getElementById('visible'),
-    rotarIzquierda : document.getElementById('rotarIzquierda'),
-    rotarDerecha : document.getElementById('rotarDerecha'),
-    masTransparente : document.getElementById('masTransparente'),
-    menosTransparente : document.getElementById('menosTransparente'),
-    sombraOn : document.getElementById('sombraOn'),
-    sombraOff : document.getElementById('sombraOff')
-}
-botones.izquierda.addEventListener('click', izquierdaHandler, false);
-botones.derecha.addEventListener('click', derechaHandler, false);
-botones.arriba.addEventListener('click', arribaHandler, false);
-botones.abajo.addEventListener('click', abajoHandler, false);
-botones.agranda.addEventListener('click', agrandaHandler, false);
-botones.achica.addEventListener('click', achicaHandler, false);
-botones.invisible.addEventListener('click', invisibleHandler, false);
-botones.visible.addEventListener('click', visibleHandler, false);
-botones.rotarIzquierda.addEventListener('click', rotarIzquierdaHandler, false);
-botones.rotarDerecha.addEventListener('click', rotarDerechaHandler, false);
-botones.masTransparente.addEventListener('click', masTransparenteHandler, false);
-botones.menosTransparente.addEventListener('click', menosTransparenteHandler, false);
-botones.sombraOn.addEventListener('click', sombraOnHandler, false);
-botones.sombraOff.addEventListener('click', sombraOffHandler, false);
-
-function sombraOnHandler(){
-    cat.sombra = true;
-}
-
-function sombraOffHandler(){
-    cat.sombra = false;
-}
-
-function masTransparenteHandler(){
-    if(cat.alpha > 0.1){
-        cat.alpha -= 0.1;
-    }
-}
-
-function menosTransparenteHandler(){
-    if(cat.alpha < 1){
-        cat.alpha += 0.1;
-    }
-}
-
-function rotarIzquierdaHandler(){
-    cat.rotar -= 10;
-}
-
-function rotarDerechaHandler(){
-    cat.rotar += 10;
-}
-
-function izquierdaHandler(){
-    cat.x -= 4;
-}
-
-function derechaHandler(){
-    cat.x += 4;
-}
-
-function arribaHandler(){
-    cat.y -= 4;
-}
-
-function abajoHandler(){
-    cat.y += 4;
-}
-
-function agrandaHandler(){
-    cat.width += 10;
-    cat.height += 10;
-    cat.x -= 5;
-    cat.y -= 5;
-}
-
-function achicaHandler(){
-    cat.width -= 10;
-    cat.height -= 10;
-    cat.x += 5;
-    cat.y += 5;
-}
-
-function invisibleHandler(){
-    cat.visible = false;
-}
-function visibleHandler(){
-    cat.visible = true;
-}
-
-function loadHandler(){
-    //imagenesCargadas se asegura de que todas las imagenes del juego sean cargadas
-    //antes de que el juego comienze eliminando la posibilidad de que 
-    //alguna imagen no sea cargada antes de que el codigo corra.
-    imagenesCargadas += 1;
-    if (imagenesCargadas === 1) {
-        update();
-    }
-}
-
-function update(){
-    frame(update);
-    render();
-}
-
-function render(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    
-    if (sprites.length !== 0) {
-        for (let i = 0; i < sprites.length; i++) {
-            let sprite = sprites[i];
-            if (sprite.visible) {
-                context.save();
-                context.translate(
-                    Math.floor(sprite.x + (sprite.width/ 2)),
-                    Math.floor(sprite.y + (sprite.height/ 2))
-                )
-
-                context.rotate(Math.PI / 180 * sprite.rotar);
-                
-                context.globalAlpha=sprite.alpha;
-
-                if (sprite.sombra) {
-                    context.shadowColor='rgba(100,100,100,0.5)';
-                    context.shadowOffsetX= 3;
-                    context.shadowOffsetY= 3;
-                    context.shadowBlur= 3;
-                }
-
-                context.drawImage(image,
-                            sprite.sourceX, sprite.sourceY,
-                            sprite.sourceWidth, sprite.sourceHeight,
-                            Math.floor(-sprite.width/2), Math.floor(-sprite.height/2),sprite.width,sprite.height);
-                
-                context.restore();
-                
-            }
-        }
-    }
-}
+//evento al precionar teclas para mover el sprite.
+window.addEventListener('keydown', manejadorJuego.keyDownHandler, false);
+window.addEventListener('keyup', manejadorJuego.keyUpHandler, false);
